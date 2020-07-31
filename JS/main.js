079c91a1b74618bb8a68bf5f5e8aee7a7c28d3ec
Jasvi-Chahal <58879701+Jasvi-Chahal@users.noreply.github.com>
2020-07-31 15:58:44 +1000

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+//sticky nav bar
+window.onscroll = function() { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky")
+    }
+}
+
+//owl caraosel for blog
+$(".owl-carousel").owlCarousel();
+
+$('.single-item').slick();
+
+//form confirmation
+function myFunction() {
+    confirm("We received your form. we will contact you soon! Thanks!");
+}
