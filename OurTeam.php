e99107be280c4fdb3650d9fef5c8445eacbff187
ashmita <46643295+asmi122@users.noreply.github.com>
2020-08-06 21:15:55 +1000

    our-team

---
+++ OurTeam.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="CSS/mystyle.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>   
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <title>Adventure Kora</title>
+  </head>
+  <body>
+   <!-- header: logo and navigation-menu -->
+   <?php 
+    include('HeaderFooter/header.php'); 
+    ?>
+    <!-- end header -->
+    <div class="our-story">
+      <h1> Adventure Kora - Our Team</h1>
+
+      <section id="team1">
+        <img src="images\team1.jpg"> 
+        <p> <br/> Experience adventure with mindfulness traveling with mindfulness gives an enriching experience. You start appreciating a culture, a place, and its people from a different light. </p>
+
+      </section>
+
+      <section id="team2">
+        <img src="images\team2.jpg">
+        <p><br/>Adventure Kora is a local travel company based in Nepal that specializes in small groups, bespoke packages, combining them with the idea of wellness. We believe in creating the best travel experience for our clients, their safety being the main priority. </p>
+
+        
+      </section>
+      
+    </div>
+
+
+
+ 
+            <!-- Footer Start -->
+	<?php 
+    include('HeaderFooter/footer.php'); 
+    ?>
+    <!-- Footer End -->
+  </body>
+</html>
