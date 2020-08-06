850b2c0c5438323641827cd41b236aaa8de33932
Manpreet Braich <manpreetbenipal07@gmail.com>
2020-08-06 10:53:22 +1000

    contact us form with map and mailing option

--- ContactUs.php
+++ ContactUs.php
@@ -24,39 +24,121 @@
     include('HeaderFooter/header.php'); 
     ?>
     <!-- end header -->
- <!-- Top content -->
- <!--Contact form start-->
- 
- <div class="contact"> 
-	 <h1>CONTACT US</h1>
-	<div class="text">
-		<p class="para"> We have lots of useful information in the Quick Links section at the bottom of each page of our website. If you require further information, wish to plan your trip, or want to ask any question, please fill out the form below and we will aim to respond to your enquiry in business days.</p>
-	</div>
-	<div>				
-		<div class="contact-form">
-			<label> Full Name  </label> </br>
-			<input type="text" name="" value="" placeholder="Enter your name">
+ <!--Cntact-form-->
+ <div class="contact-us">
+ 	<!--google-map-->
+ <div class="container">
+	<div class="row">
+	<br><br>
+			<div class="map-holder">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14126.970326011877!2d85.33567660848618!3d27.725233163440652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19693c88645f%3A0xbf69e587bc07a866!2sDhumbarahi%2C+Kathmandu+44600!5e0!3m2!1sen!2snp!4v1517207791266"
+                    width="1150" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+		
 		</div>
-		<div class="contact-form">
-			<label> Email Address </label> </br>
-			<input type="email" name="" value="" placeholder="Enter your Email">
-		</div>
-		<div class="contact-form">
-			<label> Country </label> </br>
-			<input type="text" name="" value="" placeholder="Enter Your Country">
-		</div>
-		<div class="contact-form">
-			<label> Message </label> </br>
-			<textarea></textarea>
-		</div>
-		<a class="button" href=""> SUBMIT </a>
-	</div>
-	<div class="text">
-		<p>You can also contact us through our E-mail: adventurekora@gmail.com</p>
-	</div>
-</div>
+	<!--Section:-Contact form-->
+	<div class="card">
+		<section class="mb-4">
+		<!--Section heading-->
+			<h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+		<!--Section description-->
+			<p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
+			a matter of hours to help you.</p>
+		<div class="row">
+		<!--Grid column-->
+        <div class="col-md-8 mb-md-0 mb-6">
+            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
 
-  <!--Contact form end-->
+                <!--Grid row-->
+                <div class="row" style="padding-left:35px">
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+							<label for="name" class="">Your name</label>
+                            <input type="text" id="name" name="name" class="form-control">
+                            
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+							<label for="email" class="">Your email</label>
+                            <input type="text" id="email" name="email" class="form-control">
+                            
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row" style="padding-left:35px">
+                    <div class="col-md-12">
+                        <div class="md-form mb-0">
+							<label for="subject" class="">Subject</label>
+                            <input type="text" id="subject" name="subject" class="form-control">
+                            
+                        </div>
+                    </div>
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row" style="padding-left:35px">
+
+                    <!--Grid column-->
+                    <div class="col-md-12">
+
+                        <div class="md-form">
+							<label for="message">Your message</label>
+                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                            
+                        </div>
+
+                    </div>
+                </div>
+                <!--Grid row-->
+
+            </form><br>
+
+
+            <div class="text-center text-md-left" style="padding-left:35px">
+               <a><button type="button" class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</button></a>
+            </div>
+            <div class="status"></div>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-md-4 text-center">
+            <ul class="list-unstyled mb-0">
+                <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                    <p>Dhumbarahi Kathmandu 44600</p>
+                </li>
+
+                <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                    <p> <a href="tel:+9779849590899" onclick="ga('send', 'event', { eventCategory: 'Contact', eventAction: 'Call', eventLabel: 'Mobile Button'});"><p class="call-button">+9779849590899</p></a></p>
+                </li>
+
+                <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                    <p>adventurekora@gmail.com</p>
+                </li>
+            </ul>
+        </div>
+        <!--Grid column-->
+
+    </div>
+
+</section>
+<!--Section: Contact v.2-->
+    </div>
+    </div>
+	</div>
+<!--Contact form end-->
             <!-- Footer Start -->
 	<?php 
     include('HeaderFooter/footer.php'); 
