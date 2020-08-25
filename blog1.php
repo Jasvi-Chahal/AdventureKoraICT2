f9262203865d125754192afc03bb7cc8365014ce
ashmita <46643295+asmi122@users.noreply.github.com>
2020-08-25 21:49:14 +1000

    blogs

---
+++ blog1.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="CSS/mystyle.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>   
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <title>Adventure Kora</title>
+  </head>
+  <body>
+   <!-- header: logo and navigation-menu -->
+   <?php 
+    include('HeaderFooter/header.php'); 
+    ?>
+    <!-- end header -->
+    <div class="our-story">
+      <h1> Adventure Kora - Our Story</h1>
+
+      <section id="our-story1">
+        <img src="images\story.jpg">
+
+      </section>
+
+      <section id="our-story2">
+        <p>ATrekking Mount Everest 
+
+Well, one of the most famous natural icons of Nepal is Mount Everest which is also one of the most adventurous one. Nepal is known one of the best countries for trekking, not just the Everest but other peaks too. Every year thousands of trekkers and climbers come to Nepal to enjoy this serene beauty and test their endurance. The best part is the government of Nepal is continuously opening new trekking areas that were restricted earlier, means you can try a new route every time you visit Nepal. If you are up for a challenging adventure, then try Everest Base Camp Trek that involves (number of days) of trekking along the rocky trails and snow-capped peaks. You will need both mental and physical ability to experience the feeling of conquering this King of Mountains. 
+
+Starting point: Well commonly this trips starts from the town of Lukla, but there are many other options, depending on the tour and difficulty you want to endure for this journey. 
+
+Bungee Jumping & Canyon Swing 
+
+Nepal has been labeled as one of the best sites to experience the bungee jump. You can experience this thrilling jump at around 160 meters over Bhotekhosi, one of the fastest flowing rivers in Nepal. Follow this up with the canyon swing of 240-meter free fall at the speed of 150 meters per hour. Currently, there are two bungy spots in Nepal one at Pokhara and other at Bhotekoshi.
+
+Starting point: Well you can contact your tour agency to for further information as they provide pick and drop service as well. 
+
+Ultra Light Flying 
+
+This activity involves microlight aircraft that takes you around Nepal’s mountains letting you see the spectacular view of the mountain ranges with an extra thrill. It is an exciting 90 minutes experience where you can enjoy the natural beauty of Mountains that will always be in your memory train. 
+
+Starting point: This service operates only in Pokhara airport. You can contact your tour guide for booking. 
+
+Jungle Safari 
+
+Apart from the breathtaking mountains, Nepal is also famous for its luscious tropical Jungle situated in the southern part of the country, home to various exotic animals.  One of the favorite activity in Nepal preferred by the wild lovers is Jungle Safari. During this activity, you can observe the wild animals in their natural habitat. If you are a lover of wilderness Nepal is a place to be. Nepal has multiple national parks which have various wild and exotic animals where you have multiple options of Safari to choose from. 
+Start point: You can always start your safari two well-known national parks of Nepal which are Chitwan National Park and Bardiya National Park. If you want to get the best experience out of the Safari tours, it is best advised to get it from the best touring company. 
+
+Zip flyer 
+
+Zip line in Nepal is one of the tallest longest and steepest one in the whole world. It has the length of 1.8 km with the incline of  56 degrees and a vertical drop of 600 meters. Renowned for being one of the extreme zip lines you can feel the adrenaline rush when you go forward to the ground and experience the beautiful scenery around you. 
+
+Where to do it: Zip line experience is available in Pokhara.  
+</p>
+        
+      </section>
+      
+    </div>
+
+
+
+ 
+            <!-- Footer Start -->
+	<?php 
+    include('HeaderFooter/footer.php'); 
+    ?>
+    <!-- Footer End -->
+  </body>
+</html>
