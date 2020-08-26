06146ea7a8f84c9a7cdd6ba313784fdb9ac131fb
ashmita <46643295+asmi122@users.noreply.github.com>
2020-08-26 21:12:18 +1000

    blogs

--- blog2.php
+++ blog2.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="CSS/mystyle.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="CSS/mystyle.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -24,70 +24,75 @@
     include('HeaderFooter/header.php'); 
     ?>
     <!-- end header -->
-    <div class="our-story">
-      <h1> Adventure Kora - Our Story</h1>
+    <div class="faqHeader" style="text-align: center;">
+      <h1> Why you can’ miss the Nepalese Cuisine?</h1><br>
+        <img src="images\food.jpg" alt="Mt Everest" height="400px" width="800px" align="center">
+        <br><br>
+        </div>
+  <div class="blogs">
+  <p>      
 
-      <section id="our-story1">
-        <img src="images\story.jpg">
+<i>Buckle up, as we take you on a joyous ride of the most exotic delicacies that are bound to leave you drooling.</i><br><br><br>
 
-      </section>
-
-      <section id="our-story2">
-        <p>Buckle up, as we take you on a joyous ride of the most exotic delicacies that are bound to leave you drooling.
-
-Momos
-
+<b>Momos</b> <br><br>
 
 No list of flavorsome Nepali food is complete without this ultimate favorite. Momos are juicy dumplings made in white flour, with delicious stuffing, cooked over steam and served to you with the most finely cooked chutney.
-And in the modern food definition, they are the healthiest form of a scrumptious delight. Dumplings are filled with meat stuffing, buffalo meat being a cult favorite (others are equally delicious though, like chicken, and even vegetables for that matter). They are served to you fried, or with gravy (known as jhol momos). But trust us as we say it, momos are delicious in every form.
+And in the modern food definition, they are the healthiest form of a scrumptious delight. Dumplings are filled with meat stuffing, buffalo meat being a cult favorite (others are equally delicious though, like chicken, and even vegetables for that matter). They are served to you fried, or with gravy (known as jhol momos). But trust us as we say it, momos are delicious in every form.<br><br><br>
 
-Yomari
+<b>Yomari</b> <br><br>
 
 Containing sweet fillings like chaku, or the latest version with chocolate, yomari is served to with a soft sweet dip.
-Yomari is usually enjoyed with great joy on the occasion of Yomari Punhi and takes you on a much needed sweet journey.
+Yomari is usually enjoyed with great joy on the occasion of Yomari Punhi and takes you on a much needed sweet journey. <br><br><br>
 
-Bara
+<b>Bara</b> <br><br>
 
-Bara is one of the most authentic dishes served in Nepal, and it literally spells home cooked for the people living there.
+Bara is one of the most authentic dishes served in Nepal, and it literally spells home cooked for the people living there. <br>
 
-Dark lentils are soaked overnight and then grinded to form a powder for the patties. After they are fried, minces of meat and eggs are added and moulded into a disc shape and then cooked. You will find these crispy and soft dishes only at the authentic bhattis. Making for a delightful lunch, baras are sure to keep you coming for more.
+Dark lentils are soaked overnight and then grinded to form a powder for the patties. After they are fried, minces of meat and eggs are added and moulded into a disc shape and then cooked. You will find these crispy and soft dishes only at the authentic bhattis. Making for a delightful lunch, baras are sure to keep you coming for more.<br><br><br>
 
-Tingmo and Laphing
+<b>Tingmo and Laphing</b> <br><br>
 
 Tingmo and Laphing are Tibetan dishes with a Nepali twist. 
-Soft transparent noodles cooked in hot broth,  Laphing is served with steamed bread called Tingmo. The twist lies in the part that Laphing is sweet-but-sour-but-spicy, and it takes your taste buds on a frenzy as you try to decipher the taste of it.
+Soft transparent noodles cooked in hot broth,  Laphing is served with steamed bread called Tingmo. The twist lies in the part that Laphing is sweet-but-sour-but-spicy, and it takes your taste buds on a frenzy as you try to decipher the taste of it.<br><br><br>
 
-Chatamari
+<b>Chatamari</b> <br><br>
 
 The rice flour crepe of chatamari is crispy on the outside with just right amount of chewy on the inside. Meat, egg and onions are the toppings of this food which only take it up a notch. Also known as Keema Chatamari, it is an inviting offering from the Newar community.
+<br><br><br>
 
-Thakali Thal
+<b>Thakali Thal</b> <br><br>
 
 No description of Nepali food is ever complete without the Dal-Bhat staple. And as the name suggests, Thakali thal is the Thakali take on the staple Nepali food.
 Served with fried potatoes (more like french fries, but still tastier), saag (all hail the green vegetables), pickle and chutney, black lentils and rice become even more delicious in the Thakali thal. When in Nepal, you cannot afford to miss this succulent meal with a diverse burst of flavors.
+<br><br><br>
 
-Juju Dhau
+<b>Juju Dhau</b> <br><br>
 
 When you want to escape into a creamy delight of yoghurt, Juju Dhau is the solution. Rightly known as ‘King of Curd’, the recipe for Juju Dhau has been a mystery for a long time. Made from buffalo milk and served in a clay pot, the dense creamy texture just melts into your mouth.
 Warning: It tends to be addictive.
+<br><br><br>
 
-Dhido
+<b>Dhido</b> <br><br>
 
-If you are planning to explore the mountainous region of Nepal, be prepared for this light and tasty dish called Dhido. Made with cereals and water, dhido is extremely healthy and filling at the same time. It is usually served like a typical Dal-bhat dish and you will be surprised at the nutritious goodness of the dish.
+If you are planning to explore the mountainous region of Nepal, be prepared for this light and tasty dish called Dhido. Made with cereals and water, dhido is extremely healthy and filling at the same time. It is usually served like a typical Dal-bhat dish and you will be surprised at the nutritious goodness of the dish. <br>
 
-So, that was a brief overview of all the appetizing food that awaits you in the land of mountains.
+So, that was a brief overview of all the appetizing food that awaits you in the land of mountains. <br><br>
 
-Bottom Line: You have to try them all to experience the goodness of the different flavours. </p>
+<i>Bottom Line: You have to try them all to experience the goodness of the different flavour</i>
+<br><br><br>
+
+
+
+</p>
+</div>
         
-      </section>
       
-    </div>
 
 
 
  
             <!-- Footer Start -->
-	<?php 
+  <?php 
     include('HeaderFooter/footer.php'); 
     ?>
     <!-- Footer End -->
