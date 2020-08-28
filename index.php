2c6c86156669b64f0ee05aa410da01106f1e7c5b
Manpreet Braich <manpreetbenipal07@gmail.com>
2020-08-28 15:45:21 +1000

    index page with some chnages

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
        					</ol>
        					<div class="carousel-inner">
        						<div class="carousel-item active">
-       							<img src="images/img/slide1.jpg" class="d-block w-100" alt="img1">
+       							<img src="images/img/slide1.jpg" class="d-block w-100 responsive" alt="img1">
 								<div class="carousel-caption">
 									<h1 class="wow fadeInLeftBig">A New Sky ,A New Life.</h1>
 									<div class="description wow fadeInUp">
@@ -50,7 +50,7 @@
 								</div>
        						</div>
        						<div class="carousel-item">
-       							<img src="images/img/slide2.jpg" class="d-block w-100" alt="img2">
+       							<img src="images/img/slide2.jpg" class="d-block w-100 responsive" alt="img2">
        							<div class="carousel-caption">
 									<h1 class="wow fadeInLeftBig">Tours</h1>
 									<div class="description wow fadeInUp">
@@ -62,7 +62,7 @@
 								</div>
        						</div>
        						<div class="carousel-item">
-       							<img src="images/img/slide3.jpeg" class="d-block w-100" alt="img3">
+       							<img src="images/img/slide3.jpeg" class="d-block w-100 responsive" alt="img3">
        							<div class="carousel-caption">
 									<h1 class="wow fadeInLeftBig">Adventure</h1>
 									<div class="description wow fadeInUp">
@@ -86,7 +86,131 @@
        			</div>
        		</div>
         </div><hr>
+		<!------------Divider-------------->
+		<div class="divider"><span></span><span>About Us</span><span></span></div>
+		
+		<!------------------About the comopaany---------------->
+		<div class="AboutCompany">
+			<div class="container">
+				<div class="row">
+					<p>We are a Nepal based adventure company which specializes in small group bespoke travel in the Himalayas. 
+					Our itineraries offer visitors an opportunity to learn about Nepal’s unique history, to experience its local culture, 
+					and to explore the adventures Nepal has to offer alongside the idea of wellness, which is an important part of its cultural fabric.</p>
+				</div>
+			</div>
+		<div>
+		<hr>
+	<!------------Divider-------------->
+<div class="divider"><span></span><span>Featured Packages</span><span></span></div>
+<!----------------Featured package------------->
+<div class="featuredPackage">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="card">
+					<div class="image">
+						<img src="images/img/package1.jpg" alt="Package 1" width="100%" height="180px" >
+					</div>
+					<div class="card-inner">
+						<div class="header">
+							
+							<h5>Everest Base Camp Via Kongma la</h5>
+						</div>
+					<div class="content">
+						<p><i class="fas fa-calendar-day"></i> 14 Days</p>
+					</div>
+					<a href="Trekking.php"><button type="button" class="btn">Book Now</button></a>
+				</div>
+			</div>
+		</div>
+    <div class="col-sm-4">
+		<div class="card">
+			<div class="image">
+				<img src="images/img/package2.jpg" alt="Package 2" width="100%" height="180px">
+			</div>
+			<div class="card-inner">
+				<div class="header">
+					
+					<h5>Everest Base Camp with Gokyo Lake</h5>
+				</div>
+				<div class="content">
+					<p><i class="fas fa-calendar-day"></i> 15 Days</p>
+				</div>
+				<a href="Trekking.php"><button type="button" class="btn">Book Now</button></a>
+		</div>
+	</div>
+</div>
+<div class="col-sm-4">
+  <div class="card">
+    <div class="image">
+      <img src="images/img/package3.jpg" alt="Package 3" width="100%" height="180px"> 
+	</div>
+    <div class="card-inner">
+      <div class="header">
+	  <h5>Everest Base Camp</h5>
+    </div>
+    <div class="content">
+      <p><i class="fas fa-calendar-day"></i> 12 Days</p>
+    </div>
+	<a href="Trekking.php"><button type="button" class="btn">Book Now</button></a>
+    </div>
+  </div>
+</div>
+</div><br><br>
+<div class="row">
+		<div class="col-sm-4">
+		  <div class="card">
+			<div class="image">
+			  <img src="images/img/package4.jpg" alt="Package 4" width="100%" height="180px">
+			</div>
+			<div class="card-inner">
+			  <div class="header">
+			  <h5>Everest Three Passes</h5>
+			</div>
+			<div class="content">
+			  <p><i class="fas fa-calendar-day"></i> 18 Days</p>
+			</div>
+			<a href="Trekking.php"><button type="button" class="btn">Book Now</button></a>
+			  </div>
+		  </div>
+		</div><div class="col-sm-4">
+		  <div class="card">
+			<div class="image">
+			  <img src="images/img/package5.jpg" alt="Package 5" width="100%" height="180px">
+			</div>
+			<div class="card-inner">
+			  <div class="header">
+			  <h5>Gokyo Lake Trek-12</h5>
+			</div>
+			<div class="content">
+			  <p><i class="fas fa-calendar-day"></i> 12 Days</p>
+			</div>
+			<a href="Trekking.php"><button type="button" class="btn">Book Now</button></a>
+			  </div>
+		  </div>
+		</div>
+		<div class="col-sm-4">
+		  <div class="card">
+			<div class="image">
+			  <img src="images/img/package6.jpg" alt="Package 6" width="100%" height="180px">
+			</div>
+			<div class="card-inner">
+			  <div class="header">
+			  <h5>Gokyo Lake with Renjo La</h5>
+			</div>
+			<div class="content">
+			 <p><i class="fas fa-calendar-day"></i> 12 Days</p>
+			</div>
+			<a href="Trekking.php"><button type="button" class="btn">Book Now</button></a>
+			  </div>
+		  </div>
+		</div>
+</div>
+</div>
+</div><hr>
+
 		<!---------------------Customise package search----------------->
+		
 
 <section class="call-to-action-box">
 			<div class="container">
@@ -152,190 +276,115 @@
 					</div><!-- row end -->
 				</div><!-- Action style box -->
 			</div><!-- Container end -->
-		</section><!-- Action end --><br>
-	<!------------Divider-------------->
-<div class="divider">
-   <div class="container-fluid">
-	   <div class="row">
-	       <h3>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp<i class="fas fa-box-open"></i>Featured Package</h3>
-        </div>
-    </div>
-</div>
-<!----------------Featured package------------->
-<div class="featuredPackage">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-4">
-				<div class="card">
-					<div class="image">
-						<img src="images/img/package1.jpg" alt="Package 1" width="100%" height="180px" >
-					</div>
-					<div class="card-inner">
-						<div class="header">
-							
-							<h5>Everest Base Camp Via Kongma la</h5>
-						</div>
-					<div class="content">
-						<p><i class="fas fa-calendar-day"></i> 14 Days</p>
-					</div>
-					<a href="Trekking.php"><button type="button" class="btn">Book Now</button></a>
-				</div>
-			</div>
-		</div>
-    <div class="col-sm-4">
-		<div class="card">
-			<div class="image">
-				<img src="images/img/package2.jpg" alt="Package 2" width="100%" height="180px">
-			</div>
-			<div class="card-inner">
-				<div class="header">
-					
-					<h5>Everest Base Camp with Gokyo Lake</h5>
-				</div>
-				<div class="content">
-					<p><i class="fas fa-calendar-day"></i> 15 Days</p>
-				</div>
-				<a href="Trekking.php"><button type="button" class="btn">Book Now</button></a>
-		</div>
-	</div>
-</div>
-<div class="col-sm-4">
-  <div class="card">
-    <div class="image">
-      <img src="images/img/package3.jpg" alt="Package 3" width="100%" height="180px"> 
-	</div>
-    <div class="card-inner">
-      <div class="header">
-	  <h5>Everest Base Camp</h5>
-    </div>
-    <div class="content">
-      <p><i class="fas fa-calendar-day"></i> 12 Days</p>
-    </div>
-	<a href="Trekking.php"><button type="button" class="btn">Book Now</button></a>
-    </div>
-  </div>
+		</section><!-- Action end -->
+		
 </div>
 
-<div class="col-sm-4">
-  <div class="card">
-    <div class="image">
-      <img src="images/img/package4.jpg" alt="Package 4" width="100%" height="180px">
-    </div>
-    <div class="card-inner">
-      <div class="header">
-	  <h5>Everest Three Passes</h5>
-    </div>
-    <div class="content">
-      <p><i class="fas fa-calendar-day"></i> 18 Days</p>
-    </div>
-	<a href="Trekking.php"><button type="button" class="btn">Book Now</button></a>
-      </div>
-  </div>
-</div><div class="col-sm-4">
-  <div class="card">
-    <div class="image">
-      <img src="images/img/package5.jpg" alt="Package 5" width="100%" height="180px">
-    </div>
-    <div class="card-inner">
-      <div class="header">
-	  <h5>Gokyo Lake Trek-12</h5>
-    </div>
-    <div class="content">
-      <p><i class="fas fa-calendar-day"></i> 12 Days</p>
-    </div>
-	<a href="Trekking.php"><button type="button" class="btn">Book Now</button></a>
-      </div>
-  </div>
-</div>
-<div class="col-sm-4">
-  <div class="card">
-    <div class="image">
-      <img src="images/img/package6.jpg" alt="Package 6" width="100%" height="180px">
-    </div>
-    <div class="card-inner">
-      <div class="header">
-	  <h5>Gokyo Lake with Renjo La</h5>
-    </div>
-    <div class="content">
-     <p><i class="fas fa-calendar-day"></i> 12 Days</p>
-    </div>
-	<a href="Trekking.php"><button type="button" class="btn">Book Now</button></a>
-      </div>
-  </div>
-</div>
-</div>
-</div><hr>
-</div>
-
-
+<hr>
 <!-----------------------divider---------------------------->
-<div class="divider">
-   <div class="container-fluid">
-	   <div class="row">
-	   <h3><i class="fas fa-box-open"></i> Why travel With Adventure</h3>
-        </div>
-    </div>
-</div>
+<div class="divider"><span></span><span>Why travel Adventure Kora</span><span></span></div>
+
 <!---------------------videp play section----------------->
 <div class="videoSection">
-	<div class="videoTextcontainer">
-		<h4>Find your adventure in Nepal</h4><br><br>
-		<a href="TailorMade.php"><button type="button" class="btn">Find your Trip</button></a>	
-	</div>
-	<div class= "iframe-container">
-	<iframe width="600px" height="355px" src="https://www.youtube.com/embed/Fty1UQamzvg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-	</div>
-</div>
-	</div>
-
-<!-----------------------divider---------------------------->
-<div class="divider">
-   <div class="container-fluid" >
-	   <div class="row" >
-	   		<h3><i class="fas fa-box-open"></i> Our Blogs</h3>
-        </div>
-    </div>
-</div>
-<!---------------------Blog post----------------->
-
-
-<div class="blog-container">
+	<div class="container">
 		<div class="row">
-			<div class="col-md-4">
-				<div class="single-blog">
-					<p class="blog-meta"> <span>May 25,2018</span></p>
-					<img src="./images/Blog1.jpg" alt="Post 1" >
-					<h2><a href="blog1.php">Adventures you should embark upon in Nepal </a></h2>
-					<p class = "blog-text">Trekking Mount Everest<br>Well, one of the most famous natural icons of Nepal is Mount Everest which is also one of the most adventurous one. </p>
-					<p><a href="blog1.php" class ="read-more-button">Read More</a>
-				<span><i class="fa fa-thumbs-o-up"></i>7 People like, <i class ="fa fa-comments-o"></i>3..</span>
-				</p>
+			<div class="col-md-5">
+				<div class="videoTextcontainer">
+					<p>
+					At Adventure Kora, we offer personalized services to all our travel partners- from leisure, 
+					culture, and adventure in the Himalayas that are tailored made according to as per their requirements.</p>
+					<a href="TailorMade.php"><button type="button" class="btn">Find your Trip</button></a>	
 				</div>
 			</div>
-		</p>
-			<div class="col-md-4">
-			<div class="single-blog">
-					<p class="blog-meta"> <span>May 25,2018</span></p>
-					<img src="./images/Blog2.jpg" alt="Post 1" >
-					<h2><a href="blog2.php">Why you can’ miss the Nepalese Cuisine?</a></h2>
-					<p class = "blog-text">Nepalese Cuisine?<br>Buckle up, as we take you on a joyous ride of the most exotic delicacies that are bound to leave you drooling.</br><br> </p>
-					<p><a href="blog2.php" class ="read-more-button">Read More</a>
-				<span><i class="fa fa-thumbs-o-up"></i>7 People like, <i class ="fa fa-comments-o"></i>3..</span>
-				</div>
-			</div>
-			<div class="col-md-4">
-			<div class="single-blog">
-					<p class="blog-meta"> <span>May 25,2018</span></p>
-					<img src="./images/Blog3.jpg" alt="Post 1" >
-					<h2><a href="blog3.php">Things you need to know before trekking to ABC_ASJ_Final </a></h2>
-					<p class = "blog-text">Annapurna Base Camp is considered to be a moderate trekking route. Compared to other trekking routes in the Himalayas, ABC trek has a lot of accommodation</p>
-					<p><a href="blog3.php" class ="read-more-button">Read More</a>
-				<span><i class="fa fa-thumbs-o-up"></i>7 People like, <i class ="fa fa-comments-o"></i>3..</span>
-				</p>
+			<div class="col-md-7">
+				<div class= "iframe-container">
+					<iframe width="600px" height="355px" src="https://www.youtube.com/embed/Fty1UQamzvg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
+<hr>
+<!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="text-center">
+          <h3>Call To Action</h3>
+          <p>Choose you new destination to discover with us.</p>
+          <a class="cta-btn" href="TailorMade.php">Call To Action</a>
+        </div>
+
+      </div>
+    </section><!-- End Cta Section -->
+<!-----------------------divider---------------------------->
+<div class="divider"><span></span><span>Our Blogs</span><span></span></div>
+<!---------------------Blog post----------------->
+ <div class="card-wrapper">
+    <div class="blog-card">
+      <div class="blog-card__thumbnail">
+        <img src="images/Blog1.jpg">
+      </div>
+
+      <div class="blog-card__details">
+        <div class="blog-card__title">
+          Adventures you should embark upon in Nepal 
+        </div>
+
+        <div class="blog-card__description">
+          Trekking Mount Everest<br>Well, one of the most famous natural icons of Nepal is Mount Everest which is also one of the most adventurous one.
+        </div>
+
+        <button class="blog-card__action"> 
+          <span style="margin-right: 10px"> Read More </span>
+          <i class="fa fa-angle-right"> </i>
+        </button>
+      </div>
+    </div>
+    
+    <div class="blog-card">
+      <div class="blog-card__thumbnail">
+        <img src="images/Blog2.jpg">
+      </div>
+
+      <div class="blog-card__details">
+        <div class="blog-card__title">
+          Why you can’ miss the Nepalese Cuisine?
+        </div>
+
+        <div class="blog-card__description">
+         Nepalese Cuisine?<br>Buckle up, as we take you on a joyous ride of the most exotic delicacies that are bound to leave you drooling.
+        </div>
+
+        <button class="blog-card__action"> 
+          <span style="margin-right: 10px"> Read More </span>
+          <i class="fa fa-angle-right"> </i>
+        </button>
+      </div>
+    </div>
+    
+    <div class="blog-card">
+      <div class="blog-card__thumbnail">
+        <img src="images/Blog3.jpg">
+      </div>
+
+      <div class="blog-card__details">
+        <div class="blog-card__title">
+          Things you need to know before trekking to ABC_ASJ_Final
+        </div>
+
+        <div class="blog-card__description">
+         Annapurna Base Camp is considered to be a moderate trekking route. Compared to other trekking routes in the Himalayas, ABC trek has a lot of accommodation.
+        </div>
+
+        <button class="blog-card__action"> 
+          <span style="margin-right: 10px"> Read More </span>
+          <i class="fa fa-angle-right"> </i>
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <!-----------------------divider---------------------------->
 
